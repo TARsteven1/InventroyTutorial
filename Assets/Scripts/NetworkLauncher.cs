14aaa251bc499467f27ddcda8bb95fc7a6cb1085
TAR-Home <764521788@qq.com>
2021-10-11 20:44:59 +0800

    network

--- Assets/Scripts/NetworkLauncher.cs
+++ Assets/Scripts/NetworkLauncher.cs
@@ -11,6 +11,8 @@ public class NetworkLauncher : MonoBehaviourPunCallbacks
     public GameObject NameUI;
     public InputField NameField;
     public InputField RoomField;
+
+    public GameObject roomLostUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,7 @@ public class NetworkLauncher : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         NameUI.SetActive(true);
+        PhotonNetwork.JoinLobby();
             //base.OnConnectedToMaster();
 
         //创建房间
@@ -28,10 +31,14 @@ public class NetworkLauncher : MonoBehaviourPunCallbacks
     public void OnCreateRoom()
     {
         // base.OnJoinedRoom();
-        if (RoomField.text.Length<2)
+        if (RoomField.text.Length < 2) {
+            RoomField.transform.GetChild(1).GetComponent<Text>().color = Color.red;
+            RoomField.transform.GetChild(1).GetComponent<Text>().text = "请输入房间名";
+            LoginUI.transform.GetChild(2).gameObject.SetActive(true);
             return;
-        
+    }
         LoginUI.SetActive(false);
+        roomLostUI.SetActive(false);
         RoomOptions roomOptions = new RoomOptions { MaxPlayers = 20 };
         PhotonNetwork.JoinOrCreateRoom(RoomField.text, roomOptions, default);
        // PhotonNetwork.Instantiate("Player",new Vector3(1,1,0),Quaternion.identity,0);
@@ -48,9 +55,20 @@ public class NetworkLauncher : MonoBehaviourPunCallbacks
     }
     public void PlayeButton()
     {
+        if (NameField.text.Length < 2)
+        {
+            NameField.transform.GetChild(1).GetComponent<Text>().color = Color.red;
+            NameField.transform.GetChild(1).GetComponent<Text>().text = "请输入名字";
+            NameUI.transform.GetChild(2).gameObject.SetActive(true);
+            return;
+        }
         NameUI.SetActive(false);
         PhotonNetwork.NickName = NameField.text;
         LoginUI.SetActive(true);
+        if (PhotonNetwork.InLobby)
+        {
+            roomLostUI.SetActive(true);
+        }
     }
     
 }
---
+++ Assets/Scripts/RoomListManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomListManager : MonoBehaviourPunCallbacks
+{
+    public GameObject roomNamePrafab;
+    public Transform gridLayout;
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        for (int i = 0; i < gridLayout.childCount; i++)
+        {
+            if (gridLayout.GetChild(i).gameObject.GetComponentInChildren<Text>().text==roomList[i].Name)
+            {
+                Destroy(gridLayout.GetChild(i).gameObject);
+                if (roomList[i].PlayerCount==0)
+                {
+                    roomList.Remove(roomList[i]);
+                }
+            }
+        }
+        //base.OnRoomListUpdate(roomList);
+        foreach (var room in roomList)
+        {
+            GameObject newRoom = Instantiate(roomNamePrafab, gridLayout.transform.position,Quaternion.identity);
+            newRoom.GetComponentInChildren<Text>().text = room.Name+"("+room.PlayerCount+")";
+            newRoom.transform.SetParent(gridLayout);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
